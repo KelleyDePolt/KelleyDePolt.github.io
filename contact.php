3f52482e6a3a04b75c4274b79cff064605221784
KelleyDePolt <77862359+KelleyDePolt@users.noreply.github.com>
2021-01-25 20:35:29 -0500

    added more php functionality

--- contact.php
+++ contact.php
@@ -10,6 +10,6 @@ $mailheader = "From: $email \r\n";
 
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 
-echo "Thank You!";
+echo "Thank You!" . " -" . "<a href='index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
 
 ?>
